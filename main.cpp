53ebf37f3f428c074a0328b86bd9a330ae9046b6
Zaine <74148219+rhinorunner@users.noreply.github.com>
2022-10-27 13:55:25 +0000

    kys

--- main.cpp
+++ main.cpp
@@ -1,18 +1,21 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <climits>
 #include "lib.h"
 
-void clear() {std::cout << "\033[2J\033[H\n";}
+//void clear() {std::cout << "\033[2J\033[H\n";}
+void clear() {}
 
 template <typename T>
 T input(const std::string& prompt, const std::string errorMsg = "invalid input",const uint8_t afterFail = 0) {
 	T var;
 	std::cout << prompt;
 	std::cin >> var;
-    while(std::cin.fail())
+    while(std::cin.fail()) // incorrect user input
     {
         std::cin.clear();
-        std::cin.ignore();
+        std::cin.ignore(INT_MAX,'\n');
 		std::cout << errorMsg;
 		switch (afterFail) {
 			case 0:
@@ -43,19 +46,20 @@ int main(int argc, char *argv[]) {
 	while (start) {
 		clear();
 		uint8_t choice = input<short>("C++ MONOPOLY\n\n1. start\n2. game settings\n\n>");
-		std::cout << "input passed, returned "<<choice<<"\n";
-		getchar();
+		//std::cout << "input passed, returned "<<choice<<"\n";
+		clear();
 		switch (choice) {
 			case 1: //start
 				start = false;
 				break;
 			case 2: //game settings
 				uint8_t choice2 = input<short>("GAME SETTINGS:\n1. taxes go to \"pot\"       - " 
-						  + thisGame.playWithPot 
+						  + std::to_string(thisGame.playWithPot)
 						  + "\n2. free parking gets \"pot\" - " 
-						  + thisGame.freeParkingGetsPot 
+						  + std::to_string(thisGame.freeParkingGetsPot)
 						  + "\n3. $400 if landed on go    - " 
-						  + thisGame.landOnGoGet400 << "\n4. main menu\n\nEnter a number to edit\n> ");
+						  + std::to_string(thisGame.landOnGoGet400)
+						  + "\n4. main menu\n\nEnter a number to edit\n> ");
 		}
 	}
 	return 0;
